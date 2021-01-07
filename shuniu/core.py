96af491033f4957f593c586dc60c0f1e61325742
scp10011 <506775557@qq.com>
2021-01-07 15:26:59 +0800

    修饰器缺少参数

--- shuniu/core.py
+++ shuniu/core.py
@@ -50,9 +50,9 @@ class Singleton(object):
         self._cls = cls
         self._instance = {}
 
-    def __call__(self):
+    def __call__(self, *args, **kwargs):
         if self._cls not in self._instance:
-            self._instance[self._cls] = self._cls()
+            self._instance[self._cls] = self._cls(*args, **kwargs)
         return self._instance[self._cls]
 
 
