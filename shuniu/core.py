87031004b6f7ccef2d356974ec2f70ecd85f9ae0
scp10011 <506775557@qq.com>
2021-01-20 11:46:10 +0800

    变更同步方法

--- shuniu/core.py
+++ shuniu/core.py
@@ -452,13 +452,12 @@ class Shuniu:
     def signature(self, name: str) -> "Signature":
         return Signature(self.rpc, name)
 
-    def worker(self, queue: multiprocessing.Queue, wid: int):
+    def worker(self, stdin: multiprocessing.Queue, stdout: multiprocessing.Queue, wid: int):
         self.fork()
         while 1:
             with contextlib.suppress(Exception):
-                task = queue.get()
-                if not task:
-                    continue
+                stdout.put(1)
+                task = stdin.get()
                 kwargs, task_id, src, task_type = task
                 worker_class = self.task_registered_map[task_type]
                 if not worker_class.forked:
@@ -549,13 +548,13 @@ class Shuniu:
             print(f".> {self.rpc.task_map[tid]} -- ignore_result: {task.ignore_result}")
 
     def daemon(self):
-        for wid, (worker, queue) in self.worker_pool.items():
+        for wid, (worker, stdin, stdout) in self.worker_pool.items():
             try:
                 worker.join(timeout=0)
                 if not worker.is_alive():
                     self.logger.error(f"worker {wid} is down..")
-                    worker = multiprocessing.Process(target=self.worker, args=(queue, wid,))
-                    self.worker_pool[wid] = (worker, queue)
+                    worker = multiprocessing.Process(target=self.worker, args=(stdin, stdout, wid,))
+                    self.worker_pool[wid] = (worker, stdin, stdout)
             except Exception:
                 pass
         time.sleep(1)
@@ -566,35 +565,35 @@ class Shuniu:
             threading.Thread(target=self.manager_worker).start()
         threading.Thread(target=self.ack_worker).start()
         for i in range(self.conf["concurrency"]):
-            queue = multiprocessing.Queue()
-            worker = multiprocessing.Process(target=self.worker, args=(queue, i,))
-            self.worker_pool[i] = (worker, queue)
+            stdin = multiprocessing.Queue()
+            stdout = multiprocessing.Queue()
+            worker = multiprocessing.Process(target=self.worker, args=(stdin, stdout, i,))
+            self.worker_pool[i] = (worker, stdin, stdout)
             worker.start()
         threading.Thread(target=self.daemon).start()
         self.print_banners()
         while 1:
-            for wid, (worker, queue) in self.worker_pool.items():
+            for wid, (worker, stdin, stdout) in self.worker_pool.items():
                 try:
-                    tmp = queue.get_nowait()
-                    queue.put(tmp)
+                    stdout.get_nowait()
                 except multiprocessing.queues.Empty:
-                    try:
-                        task = self.rpc.consume(wid)
-                    except IOError:
-                        self.logger.error("Retry after connection loss...")
-                        time.sleep(2)
-                        break
-                    except Exception:
-                        self.logger.exception("Failed to get task")
-                        break
-                    if task is EmptyData:
-                        break
-                    else:
-                        kwargs, task_id, src, task_type = task
-                        self.logger.info(f"Received task: {self.rpc.task_map[task_type]}[{task_id}]")
-                        self.state[task_type] = self.state.setdefault(task_type, 0) + 1
-                        queue.put((kwargs, task_id, src, task_type))
-                        queue.put(False)
+                    continue
+                try:
+                    task = self.rpc.consume(wid)
+                except IOError:
+                    self.logger.error("Retry after connection loss...")
+                    time.sleep(2)
+                    break
+                except Exception:
+                    self.logger.exception("Failed to get task")
+                    break
+                if task is EmptyData:
+                    break
+                else:
+                    kwargs, task_id, src, task_type = task
+                    self.logger.info(f"Received task: {self.rpc.task_map[task_type]}[{task_id}]")
+                    self.state[task_type] = self.state.setdefault(task_type, 0) + 1
+                    stdin.put((kwargs, task_id, src, task_type))
             else:
                 continue
 
