6d27c7e782c5e885d3fd418aba3f8cefd92db54c
scp10011 <506775557@qq.com>
2021-01-28 12:41:17 +0800

    添加进程ID识别

--- shuniu/core.py
+++ shuniu/core.py
@@ -70,7 +70,7 @@ def __seq__():
 
 
 def generate_distributed_id(node_id: str, role: str) -> str:
-    type_id = hashlib.md5(role.encode()).digest()[:2]
+    type_id = hashlib.md5(f"{os.getgid()}-{role}".encode()).digest()[:2]
     prefix = binascii.a2b_hex(node_id.replace("-", ""))[:4]
     ts = time.time_ns().to_bytes(8, "big")
     return str(uuid.UUID(bytes=prefix + type_id + __seq__().to_bytes(2, "big") + ts))
