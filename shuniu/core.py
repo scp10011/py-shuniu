e71cec3de1efae2512c318865a4347d71fa7d24d
scp10011 <506775557@qq.com>
2021-04-12 19:35:11 +0800

    忽略错误

--- shuniu/core.py
+++ shuniu/core.py
@@ -103,9 +103,7 @@ def decode_payload(payload: str, payload_type: int) -> Dict:
     return obj
 
 
-def encode_payload(
-    payload: Dict, coding: SerializationAlgorithm, compression: CompressionAlgorithm
-) -> (str, int):
+def encode_payload(payload: Dict, coding: SerializationAlgorithm, compression: CompressionAlgorithm) -> (str, int):
     if coding == SerializationAlgorithm.json:
         data = json.dumps(payload).encode()
     elif coding == SerializationAlgorithm.bson:
@@ -177,10 +175,7 @@ class shuniuRPC:
 
     @property
     def task_router(self):
-        return {
-            i[0].split(".", 1) if "." in i[0] else i[0]: i[1]
-            for i in self.conf["router"].items()
-        }
+        return {i[0].split(".", 1) if "." in i[0] else i[0]: i[1] for i in self.conf["router"].items()}
 
     def get_router(self, task: str) -> str:
         task_group, task_name = task.split(".")
@@ -206,38 +201,26 @@ class shuniuRPC:
             if r.ok:
                 msg = r.json()
                 if msg.get("code") != 0:
-                    raise ValueError(
-                        "Requests Error: {}".format(msg.get("msg", ""))
-                    ) from None
+                    raise ValueError("Requests Error: {}".format(msg.get("msg", ""))) from None
                 self.logged_in = True
                 return
             else:
-                raise ConnectionError(
-                    "connection to rpc server error: {}".format(r.status_code)
-                ) from None
+                raise ConnectionError("connection to rpc server error: {}".format(r.status_code)) from None
 
     def get_task_list(self):
         with self.__api_call__("GET", "registered/task") as r:
             if r.ok and r.json().get("code") == 0:
-                self.task_map.update(
-                    {task["name"]: task["tid"] for task in r.json().get("data")}
-                )
-                self.task_map.update(
-                    {task["tid"]: task["name"] for task in r.json().get("data")}
-                )
+                self.task_map.update({task["name"]: task["tid"] for task in r.json().get("data")})
+                self.task_map.update({task["tid"]: task["name"] for task in r.json().get("data")})
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("msg", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("msg", "") or r.status_code)
                 ) from None
 
     def registered(self, name: str) -> int:
         if name in self.task_map:
             return self.task_map[name]
-        if not isinstance(name, str) or not re.match(
-            "^([a-zA-Z_]+\\.[a-zA-Z_]+)$", name
-        ):
+        if not isinstance(name, str) or not re.match("^([a-zA-Z_]+\\.[a-zA-Z_]+)$", name):
             raise ValueError("Task Name Illegal")
         with self.__api_call__("POST", f"registered/task/{name}") as r:
             if r.ok and r.json().get("code") == 0:
@@ -246,9 +229,7 @@ class shuniuRPC:
                 return tid
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("msg", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("msg", "") or r.status_code)
                 ) from None
 
     def consume(self, worker_id):
@@ -264,11 +245,7 @@ class shuniuRPC:
                         data["src"],
                         data["type_id"],
                     )
-            raise ValueError(
-                "Requests Error: {}".format(
-                    r.ok and r.json().get("msg", "") or r.status_code
-                )
-            ) from None
+            raise ValueError("Requests Error: {}".format(r.ok and r.json().get("msg", "") or r.status_code)) from None
 
     def apply_async(
         self,
@@ -293,11 +270,7 @@ class shuniuRPC:
             queue = str(uuid.UUID(queue))
         except (ValueError, AttributeError):
             raise ValueError("Queue is illegal!") from None
-        task_id = (
-            uuid.UUID(options["task_id"]).hex
-            if "task_id" in options
-            else uuid.uuid5(self.uid, uuid.uuid4().hex)
-        )
+        task_id = uuid.UUID(options["task_id"]).hex if "task_id" in options else uuid.uuid5(self.uid, uuid.uuid4().hex)
         task_id = str(task_id)
         payload, payload_type = encode_payload(
             {"args": list(args), "kwargs": kwargs, **options},
@@ -319,9 +292,7 @@ class shuniuRPC:
                 return AsyncResult(task_id, self)
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def broadcast(
@@ -343,11 +314,7 @@ class shuniuRPC:
         kwargs = kwargs or {}
         if not destinations:
             raise ValueError("Destination cannot be empty")
-        task_id = (
-            uuid.UUID(options["task_id"]).hex
-            if "task_id" in options
-            else uuid.uuid5(self.uid, uuid.uuid4().hex)
-        )
+        task_id = uuid.UUID(options["task_id"]).hex if "task_id" in options else uuid.uuid5(self.uid, uuid.uuid4().hex)
         task_id = str(task_id)
         payload, payload_type = encode_payload(
             {"args": list(args), "kwargs": kwargs},
@@ -370,9 +337,7 @@ class shuniuRPC:
                 return r.json()["map"]
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def ack(self, task_id: str, fail: bool = False, retry: bool = False):
@@ -384,14 +349,10 @@ class shuniuRPC:
                 return
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
-    def set(
-        self, task_id: str, src: str, payload: Any, serialization=None, compression=None
-    ):
+    def set(self, task_id: str, src: str, payload: Any, serialization=None, compression=None):
         payload, payload_type = encode_payload(
             payload,
             coding=serialization or self.conf["serialization"],
@@ -403,9 +364,7 @@ class shuniuRPC:
                 return
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def get(self, task_id: str):
@@ -418,9 +377,7 @@ class shuniuRPC:
                     return decode_payload(data["payload"], data["type"])
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def revoke(self, task_id: str):
@@ -429,9 +386,7 @@ class shuniuRPC:
                 return
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def pause(self, task_id: str):
@@ -440,9 +395,7 @@ class shuniuRPC:
                 return
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def restore(self, task_id: str):
@@ -451,9 +404,7 @@ class shuniuRPC:
                 return
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def many_revoke(self, tids):
@@ -462,9 +413,7 @@ class shuniuRPC:
                 return r.json().get("state")
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def many_pause(self, tids):
@@ -473,9 +422,7 @@ class shuniuRPC:
                 return r.json().get("state")
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def many_restore(self, tids):
@@ -484,9 +431,7 @@ class shuniuRPC:
                 return r.json().get("state")
             else:
                 raise ValueError(
-                    "Requests Error: {}".format(
-                        r.ok and r.json().get("code", "") or r.status_code
-                    )
+                    "Requests Error: {}".format(r.ok and r.json().get("code", "") or r.status_code)
                 ) from None
 
     def manager(self):
@@ -500,11 +445,7 @@ class shuniuRPC:
                         decode_payload(data["payload"], data["type"]),
                         data["instruction"],
                     )
-            raise ValueError(
-                "Requests Error: {}".format(
-                    r.ok and r.json().get("msg", "") or r.status_code
-                )
-            ) from None
+            raise ValueError("Requests Error: {}".format(r.ok and r.json().get("msg", "") or r.status_code)) from None
 
     def router(self, channel):
         with self.__api_call__("POST", "/router", data={"dst": list(channel)}) as r:
@@ -512,11 +453,7 @@ class shuniuRPC:
                 data = r.json()
                 if data["code"] == 0:
                     return data["queue"]
-            raise ValueError(
-                "Requests Error: {}".format(
-                    r.ok and r.json().get("msg", "") or r.status_code
-                )
-            ) from None
+            raise ValueError("Requests Error: {}".format(r.ok and r.json().get("msg", "") or r.status_code)) from None
 
 
 @contextlib.contextmanager
@@ -531,9 +468,7 @@ def nonblocking(lock):
 
 def urlparse(uri) -> Dict:
     obj = urllib.parse.urlparse(uri)
-    url = urllib.parse.urlunparse(
-        [obj.scheme, obj.netloc.split("@")[-1], obj.path, "", "", ""]
-    )
+    url = urllib.parse.urlunparse([obj.scheme, obj.netloc.split("@")[-1], obj.path, "", "", ""])
     return {"username": obj.username, "password": obj.password, "url": url}
 
 
@@ -621,16 +556,12 @@ class Shuniu:
             worker_base = base
         else:
             worker_base = Task
-        self.task_registered_map[type_id] = worker_base(
-            app=self, name=name, func=func, conf=self.conf, **kwargs
-        )
+        self.task_registered_map[type_id] = worker_base(app=self, name=name, func=func, conf=self.conf, **kwargs)
 
     def signature(self, name: str) -> "Signature":
         return Signature(self.rpc, name)
 
-    def worker(
-        self, stdin: multiprocessing.Queue, wid: int, lock: multiprocessing.Lock
-    ):
+    def worker(self, stdin: multiprocessing.Queue, wid: int, lock: multiprocessing.Lock):
         self.fork()
         task_end = set()
         self.logger.info("Fork shuniu connect")
@@ -738,20 +669,17 @@ class Shuniu:
     def daemon(self):
         while 1:
             for wid, (worker, stdin, lock) in self.worker_pool.items():
-                try:
+                with contextlib.suppress(Exception):
                     worker.join(timeout=0)
                     if not worker.is_alive():
                         self.logger.error(f"worker {wid} is down..")
-                        worker = multiprocessing.Process(
-                            target=self.worker, args=(stdin, wid, lock)
-                        )
+                        worker = multiprocessing.Process(target=self.worker, args=(stdin, wid, lock))
                         self.worker_pool[wid] = (worker, stdin, lock)
                         worker.start()
+                with contextlib.suppress(Exception):
                     with nonblocking(lock) as locked:
                         if locked and stdin.qsize() == 0:
                             self.perform[wid] = None
-                except Exception:
-                    pass
             time.sleep(1)
 
     def start(self):
@@ -787,17 +715,13 @@ class Shuniu:
                     if task is not EmptyData:
                         kwargs, task_id, src, task_type = task
                         task_name = self.rpc.task_map[task_type]
-                        self.logger.info(
-                            f"Received task to worker-{wid}: {task_name}[{task_id}]"
-                        )
+                        self.logger.info(f"Received task to worker-{wid}: {task_name}[{task_id}]")
                         self.state[task_name] = self.state.setdefault(task_name, 0) + 1
                         self.perform[wid] = task_id
                         try:
                             stdin.put_nowait((kwargs, task_id, src, task_type))
                         except queue.Full:
-                            self.logger.error(
-                                "Failed Put {task_name}[{task_id}] to worker-{wid} Full"
-                            )
+                            self.logger.error("Failed Put {task_name}[{task_id}] to worker-{wid} Full")
                             continue
 
 
