44db2aed0ea8ac816b6be9f31ade92cbe969faf2
scp10011 <506775557@qq.com>
2021-08-06 18:48:41 +0800

    import

--- shuniu/__init__.py
+++ shuniu/__init__.py
@@ -1,20 +1,4 @@
-import binascii
-import hashlib
-import uuid
-
 from .core import Shuniu
 from .api import SerializationAlgorithm, CompressionAlgorithm, EmptyData, ErrorCode
 from .task import Task
-
-
-def __seq__():
-    sid = globals().get("__increasing__cycle__", 1)
-    globals()["__increasing__cycle__"] = 1 if sid >= 0xFFFF else sid + 1
-    return sid
-
-
-def generate_distributed_id(node_id: str, role: str) -> str:
-    type_id = hashlib.md5(f"{os.getgid()}-{role}".encode()).digest()[:2]
-    prefix = binascii.a2b_hex(node_id.replace("-", ""))[:4]
-    ts = time.time_ns().to_bytes(8, "big")
-    return str(uuid.UUID(bytes=prefix + type_id + __seq__().to_bytes(2, "big") + ts))
+from .tools import generate_distributed_id
--- shuniu/tools.py
+++ shuniu/tools.py
@@ -1,4 +1,9 @@
+import binascii
+import hashlib
 import logging
+import os
+import time
+import uuid
 
 
 class Singleton(object):
@@ -17,3 +22,16 @@ class WorkerLogFilter(logging.Filter):
         if not hasattr(record, "wid"):
             record.wid = "Main"
         return True
+
+
+def __seq__():
+    sid = globals().get("__increasing__cycle__", 1)
+    globals()["__increasing__cycle__"] = 1 if sid >= 0xFFFF else sid + 1
+    return sid
+
+
+def generate_distributed_id(node_id: str, role: str) -> str:
+    type_id = hashlib.md5(f"{os.getgid()}-{role}".encode()).digest()[:2]
+    prefix = binascii.a2b_hex(node_id.replace("-", ""))[:4]
+    ts = time.time_ns().to_bytes(8, "big")
+    return str(uuid.UUID(bytes=prefix + type_id + __seq__().to_bytes(2, "big") + ts))
