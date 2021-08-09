1acc6b11085db9df30b8f96dedcfd875dca2bab9
scp10011 <506775557@qq.com>
2021-08-09 13:32:09 +0800

    导出 Signature

--- shuniu/__init__.py
+++ shuniu/__init__.py
@@ -1,4 +1,4 @@
 from .core import Shuniu
 from .api import SerializationAlgorithm, CompressionAlgorithm, EmptyData, ErrorCode
-from .task import Task
+from .task import Task, Signature
 from .tools import generate_distributed_id
