71dfea0ddb30df13dbfa3a615dcdb5b480fe3950
scp10011 <506775557@qq.com>
2021-08-09 15:15:46 +0800

    导出异常

--- shuniu/__init__.py
+++ shuniu/__init__.py
@@ -2,3 +2,4 @@ from .core import Shuniu
 from .api import SerializationAlgorithm, CompressionAlgorithm, EmptyData, ErrorCode
 from .task import Task, Signature
 from .tools import generate_distributed_id
+from .signal_handle import ExitError, UserKillError, UserTimeoutError
